373918d049f4e8e136ab4d0d7e8e1f20027fa260
NicolasRiv24 <152893064+NicolasRiv24@users.noreply.github.com>
2023-12-04 18:18:24 -0400

    Enviroment SetUp

---
+++ src/test/java/BA3Post.java
@@ -0,0 +1,7 @@
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.Matchers.*;
+public class BA3Post {
+}
